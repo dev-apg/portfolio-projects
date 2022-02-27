32bc90cd6d192b44c9624df8e19bfee639480979
p9hy <apgitmail@gmail.com>
2022-02-27 17:13:40 +0000

    test controls pos.VI - height:100%, removed abs

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -9,12 +9,6 @@ $(window).on("load", function () {
   }
 });
 
-window.addEventListener("resize", () => {
-  document
-    .querySelector(":root")
-    .style.setProperty("--vh", window.innerHeight / 100 + "px");
-});
-
 //SET UP MAP----------------------------------------------------//
 // const map = L.map("map").setView([50, 0], 14);
 
