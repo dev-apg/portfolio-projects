abe81853f01d81260b4c216a92baf3832d197cc7
p9hy <apgitmail@gmail.com>
2022-04-19 10:07:32 +0100

    added navbar shadow

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Home Page</title>
     <link
       rel="apple-touch-icon"
       sizes="180x180"
@@ -23,8 +23,19 @@
       href="/resources/favicon-16x16.png"
     />
     <link rel="manifest" href="/site.webmanifest" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
   <body>
-    Under construction
-  </body>
-</html>
+
+    <?php
+
+echo "<h3>Site under construction</h3>";
+echo "<h3>Links:</h3>";
+echo "<a href='project1/index.html'>Project One - Gazzatteer</a>";
+
+
+
+?>
+ 
+   </body>
+ </html>
